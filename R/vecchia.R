704b0160a62b56d9f2b9f5d8916aecd2d646235c
Bradley Wakefield <bradleyw@uow.edu.au>
2026-04-30 23:27:13 +1000

    Update mG when extending groupLookup
    In setupVecchiaGeoMix (R/vecchia.R), when groupLookup contained only one column we appended a zero column but did not update mG. This change increments mG when adding the extra lookup column so the returned constants (mG) remain consistent with the modified groupLookup, preventing mismatches downstream.

--- R/vecchia.R
+++ R/vecchia.R
@@ -205,7 +205,10 @@ setupVecchiaGeoMix <- function(locations,
     groupDeps <- getDependencies(cbind(1:G,groupMatrix),G,maxDeps+1)
   }
   groupDepL <- apply(groupDeps != 0,1,sum)
-  if(ncol(groupLookup) == 1) groupLookup <- cbind(groupLookup, 0)
+  if(ncol(groupLookup) == 1){
+    groupLookup <- cbind(groupLookup, 0)
+    mG <- mG + 1
+  }
   out <- list(constants = list(m = m, G=G, mG = mG), groups = groups,
               groupNeighbours = groupNeighbours, groupMatrix = groupMatrix,
               groupLookup = groupLookup, groupNum = groupNum, groupDeps = groupDeps, groupDepL = groupDepL)
